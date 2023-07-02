2e4c0af59b51588ea99e34ed1a1bc066ccf443b7
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-07-02 15:35:27 -0300

    sequelize and swc config

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../db/sequelize/product.model";
+describe("ProductRepository", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
