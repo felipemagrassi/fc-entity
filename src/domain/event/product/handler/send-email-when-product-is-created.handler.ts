3849fe9c407ee2135eacd13a72b0bb6a173c6359
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-07-08 01:20:17 -0300

    add event-handlers

---
+++ src/domain/event/product/handler/send-email-when-product-is-created.handler.ts
@@ -0,0 +1,9 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+
+export default class SendEmailWhenProductIsCreatedHandler
+  implements EventHandlerInterface
+{
+  handle(event: any): void {
+    console.log(`Sending email to ${event.eventData.email}...`);
+  }
+}
